4031e33cc04509f1744f954f8eef45cc69c562f8
thanhnh <thanhnh99.amc@gmail.com>
2020-10-10 00:49:33 +0700

    validate

--- src/main/java/uet/japit/k62/models/request/ReqChangeAccountType.java
+++ src/main/java/uet/japit/k62/models/request/ReqChangeAccountType.java
@@ -2,8 +2,18 @@ package uet.japit.k62.models.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class ReqChangeAccountType {
+    @NotBlank(message = "userId may not be blank")
+    @NotNull(message = "userId may not be null")
+    @NotEmpty(message = "userId may not be empty")
     private String userId;
+
+
+    @NotNull(message = "accountType may not be null")
     private String accountType;
 }
