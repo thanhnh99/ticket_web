00aff15c79316ad51994891c000e5c468ea1b6bf
huent189 <hueahgase189@gmail.com>
2020-10-13 15:00:22 +0700

    update create, get voucher api

--- src/main/java/uet/japit/k62/controller/VoucherController.java
+++ src/main/java/uet/japit/k62/controller/VoucherController.java
@@ -3,13 +3,16 @@ package uet.japit.k62.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uet.japit.k62.constant.MessageConstant;
+import uet.japit.k62.constant.StatusCode;
+import uet.japit.k62.exception.exception_define.detail.InvalidConditionException;
 import uet.japit.k62.exception.exception_define.detail.VoucherHasExistedException;
+import uet.japit.k62.exception.exception_define.detail.VoucherNotFoundException;
+import uet.japit.k62.models.entity.Voucher;
 import uet.japit.k62.models.request.ReqCreateVoucher;
-import uet.japit.k62.models.response.http_response.MessageResponse;
+import uet.japit.k62.models.response.data_response.ResVoucher;
+import uet.japit.k62.models.response.http_response.HttpResponse;
 import uet.japit.k62.service.VoucherService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,8 +26,13 @@ public class VoucherController {
     @PreAuthorize("@appAuthorizer.authorize(authentication, {T(uet.japit.k62.constant.PermissionConstant).ADD_VOUCHER})")
     @PostMapping
     public ResponseEntity addVoucher(HttpServletRequest httpRequest,
-                                     @Valid @RequestBody ReqCreateVoucher requestData) throws VoucherHasExistedException {
-        MessageResponse responseData = voucherService.addVoucher(httpRequest,requestData);
+                                     @Valid @RequestBody ReqCreateVoucher requestData) throws VoucherHasExistedException, InvalidConditionException {
+        HttpResponse<Voucher> responseData = new HttpResponse<>(StatusCode.OK, MessageConstant.SUCCESS, voucherService.addVoucher(httpRequest,requestData));
+        return ResponseEntity.ok(responseData);
+    }
+    @GetMapping("/{voucher_code}")
+    public ResponseEntity getByCode(HttpServletRequest httpRequest, @PathVariable(value = "voucher_code") String code) throws VoucherNotFoundException {
+        HttpResponse<ResVoucher> responseData = new HttpResponse<>(StatusCode.OK, MessageConstant.SUCCESS, voucherService.getByCode(code));
         return ResponseEntity.ok(responseData);
     }
 }
--- src/main/java/uet/japit/k62/service/VoucherService.java
+++ src/main/java/uet/japit/k62/service/VoucherService.java
@@ -4,18 +4,21 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-import uet.japit.k62.constant.MessageConstant;
+import uet.japit.k62.dao.ICategoryDAO;
+import uet.japit.k62.dao.IEventDAO;
 import uet.japit.k62.dao.IUserDAO;
 import uet.japit.k62.dao.IVoucherDAO;
+import uet.japit.k62.exception.exception_define.detail.InvalidConditionException;
 import uet.japit.k62.exception.exception_define.detail.VoucherHasExistedException;
+import uet.japit.k62.exception.exception_define.detail.VoucherNotFoundException;
 import uet.japit.k62.models.entity.User;
 import uet.japit.k62.models.entity.Voucher;
+import uet.japit.k62.models.entity.VoucherType;
 import uet.japit.k62.models.request.ReqCreateVoucher;
-import uet.japit.k62.models.response.http_response.MessageResponse;
+import uet.japit.k62.models.response.data_response.ResVoucher;
 import uet.japit.k62.service.authorize.AttributeTokenService;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 @Service
 public class VoucherService {
@@ -23,26 +26,40 @@ public class VoucherService {
     IVoucherDAO voucherDAO;
     @Autowired
     IUserDAO userDAO;
-    public MessageResponse addVoucher(HttpServletRequest httpRequest,
-                                                   @RequestBody ReqCreateVoucher requestData) throws VoucherHasExistedException {
-        MessageResponse response = new MessageResponse();
+    @Autowired
+    IEventDAO eventDAO;
+    @Autowired
+    ICategoryDAO categoryDAO;
+    public Voucher addVoucher(HttpServletRequest httpRequest,
+                                            @RequestBody ReqCreateVoucher requestData) throws VoucherHasExistedException, InvalidConditionException {
         String token = httpRequest.getHeader("Authorization");
         String emailSendRequest = AttributeTokenService.getEmailFromToken(token);
         User userSendRequest = userDAO.findByEmail(emailSendRequest);
 
         //check voucher existed
         String voucherCode = requestData.getCode();
-        Voucher voucher = voucherDAO.findByCode(voucherCode);
-        if(voucher != null)
+
+        if(voucherDAO.findByCode(voucherCode).isPresent())
         {
             throw new VoucherHasExistedException();
         }
-        // check voucher condition valid
-        voucher = new ModelMapper().map(requestData, Voucher.class);
+
+        // check voucher condition invalid
+        if( (requestData.getType() == VoucherType.EVENT && !eventDAO.existsById(requestData.getConditionValue()))
+            || (requestData.getType() == VoucherType.CATEGORY && categoryDAO.existsById(requestData.getConditionValue()))
+            || (requestData.getType() == VoucherType.ORGANIZER && !userSendRequest.getId().equals(requestData.getConditionValue()))){
+            throw new InvalidConditionException();
+        }
+
+        Voucher voucher = new ModelMapper().map(requestData, Voucher.class);
         voucher.setCreatedBy(userSendRequest.getId());
         voucherDAO.save(voucher);
-
-        response.setMessage(MessageConstant.SUCCESS);
-        return response;
+        return voucher;
     }
+
+    public ResVoucher getByCode(String code) throws VoucherNotFoundException {
+        Voucher voucher =  voucherDAO.findByCode(code).orElseThrow(VoucherNotFoundException::new);
+        return new ModelMapper().map(voucher, ResVoucher.class);
+    }
+
 }
