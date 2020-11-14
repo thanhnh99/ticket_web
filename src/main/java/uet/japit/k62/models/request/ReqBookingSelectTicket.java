8b6080dc1add761bce1eed038defa28edd787b0a
huent189 <hueahgase189@gmail.com>
2020-11-15 02:28:35 +0700

    update booking model

--- src/main/java/uet/japit/k62/models/request/ReqBookingSelectTicket.java
+++ src/main/java/uet/japit/k62/models/request/ReqBookingSelectTicket.java
@@ -2,20 +2,22 @@ package uet.japit.k62.models.request;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class ReqBookingSelectTicket {
-    @NotNull
-    @NotBlank
-    @NotEmpty
-    private String eventId;
+//    @NotNull
+//    @NotBlank
+//    @NotEmpty
+//    private String eventId;
     @NotEmpty
     private List<ReqSelectedTicket> tickets;
     private String voucher;
+    @NotEmpty
+    @Email
     private String email;
+    @NotEmpty
     private String phone;
 }
