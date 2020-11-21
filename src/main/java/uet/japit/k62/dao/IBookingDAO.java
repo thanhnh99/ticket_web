091c0e514320e3ff60706b8f49d7341d15996899
huent189 <hueahgase189@gmail.com>
2020-11-22 00:39:57 +0700

    fixed null exception

--- src/main/java/uet/japit/k62/dao/IBookingDAO.java
+++ src/main/java/uet/japit/k62/dao/IBookingDAO.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import uet.japit.k62.models.entity.Booking;
 
 public interface IBookingDAO extends JpaRepository<Booking, String> {
-    @Query(value = "select sum(quantity) from booking, booking_detail where booking.event_id = ?1 and booking.status = 0"+
+    @Query(value = "select ifnull(sum(quantity),0) from booking, booking_detail where booking.event_id = ?1 and booking.status = 0"+
             " and TIMESTAMPDIFF(MINUTE, booking.created_at, NOW()) < 15 and booking.id = booking_detail.booking_id"+
             " and booking_detail.ticket_class_id = ?2", nativeQuery = true)
     int getNumberReservedTicket(String event_id, String ticket_id);
