00eff49663115768c936ac09f95f413b53e3ce05
huent189 <hueahgase189@gmail.com>
2020-11-23 23:41:07 +0700

    change schema

--- src/main/java/uet/japit/k62/models/entity/TicketCode.java
+++ src/main/java/uet/japit/k62/models/entity/TicketCode.java
@@ -2,13 +2,19 @@ package uet.japit.k62.models.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Data
 @AllArgsConstructor
+@Entity
+@EnableAutoConfiguration
 public class TicketCode {
+    @Id
     private String code;
     @ManyToOne
     @JoinColumn(name = "booking_id")
