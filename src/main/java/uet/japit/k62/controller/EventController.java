839f2e5f5bf081c132e6f7f7ffc73429ec0f09c8
thanhnh99 <huuthanh99hn@gmail.com>
2020-12-09 10:35:52 +0700

    fix bug CORBA

--- src/main/java/uet/japit/k62/controller/EventController.java
+++ src/main/java/uet/japit/k62/controller/EventController.java
@@ -1,7 +1,6 @@
 package uet.japit.k62.controller;
 
 
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
